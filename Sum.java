408b7c58abecec9c0b7d19e8de2408dad161c5c7
shatakshi17 <singh17shatakshi@gmail.com>
2022-06-19 22:19:37 +0530

    coded Sum.java

--- Sum.java
+++ Sum.java
@@ -1,10 +1,10 @@
 public class Sum {
     
     public static void main(String[] args) {
-        int a = 5 ;
+        int a = 3 ;
         int b = 10;
 
-        System.out.println(a+b);
+        System.out.println("The sum is : "+(a+b));
     }
     
 }
